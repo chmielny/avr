137b8a40fba858ea801bd9e24d1cbc3ea22a8a85
chmielny <michal.chmielo@gmail.com>
2018-10-13 17:50:06 +0200

    dziala 1 wersja

--- zapis_ramki/main.c
+++ zapis_ramki/main.c
@@ -5,8 +5,9 @@
 #define DATA PB1
 #define CLK PB2
 
-volatile unsigned char dataframe[7];
+volatile unsigned char dataframe[14];
 volatile unsigned int iter;
+volatile unsigned int sender_busy;
 
 void fis_start(void) {
 	// ustawienie timera 1
@@ -26,14 +27,19 @@ void fis_start(void) {
 }
 
 void fis_send_frame(void) {
+	int i;
+	for(i=0;i<7;++i)
+		dataframe[i+7] = dataframe[i];
 	iter = 0;
 	PORTB |= (1 << ENA);
 	_delay_ms(0.1);
 	PORTB &= ~(1 << ENA);
 	_delay_ms(0.1);
 	PORTB |= (1 << ENA);
+	sender_busy = 1;
 	TCCR1B |= (1 << CS10);		// bez preskalera
 	TIMSK |= (1 << OCIE1A);		// przerwanie na porownanie
+	
 }
 
 int main(void)
@@ -51,25 +57,51 @@ int main(void)
 	dataframe[4] =32;
 	dataframe[5] =32;
 	dataframe[6] =0;
-
 	fis_send_frame();
+	
+	while(sender_busy);	
+	_delay_ms(5);
+	dataframe[2] =49;
+	dataframe[3] =48;
+	dataframe[4] =51;
+	dataframe[5] =53;
+	fis_send_frame();
+	while(sender_busy);	
+	_delay_ms(5);
+	fis_send_frame();
+
 	while (1) {
 	}
 }
 
 
 ISR (TIMER1_COMPA_vect) {
-	PORTB ^= (1 << CLK);
-	if(!(iter % 2)) {
-		if ((dataframe[iter / 16] & (128 >> (iter/2) % 8)))
-			PORTB &= ~(1 << DATA);
-		else
-			PORTB |= (1 << DATA);
-	}
-	iter = iter + 1;
-	if(iter >= 112) {
+	if(iter < 112) {
+		PORTB ^= (1 << CLK);
+		if(!(iter % 2)) {
+			if ((dataframe[iter / 16] & (128 >> (iter/2) % 8)))
+				PORTB &= ~(1 << DATA);
+			else
+				PORTB |= (1 << DATA);
+		}
+	} else if (iter >= 112 && iter <= 114) {
+		PORTB &= ~(1 << ENA);
+		// czekamy
+	} else if (iter > 114 && iter <= 226) {
+		PORTB |= (1 << ENA);
+		PORTB ^= (1 << CLK);
+		if(!((iter - 114) % 2)) {
+			if ((dataframe[(iter - 114) / 16] & (128 >> ((iter - 114)/2) % 8)))
+				PORTB &= ~(1 << DATA);
+			else
+				PORTB |= (1 << DATA);
+		}
+	} else {
 		TIMSK &= ~(1 << OCIE1A);		// wyl. przerwanie na porownanie
+		TCCR1B &= ~(1 << CS10);			// wyl. zegar
 		PORTB &= ~(1 << ENA);
 		iter = 0;
+		sender_busy = 0;
 	}
+	iter = iter + 1;
 }
