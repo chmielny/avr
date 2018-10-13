95df096e787785a94a6c8d7fc87545c69ca1e84d
chmielny <michal.chmielo@gmail.com>
2018-10-13 16:23:04 +0200

    poprawki

--- zapis_ramki/main.c
+++ zapis_ramki/main.c
@@ -1,49 +1,45 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#define ENA PC0
-#define DATA PC1
-#define CLK PC2
+#define ENA PB0
+#define DATA PB1
+#define CLK PB2
 
 volatile unsigned char dataframe[7];
-volatile unsigned char iter;
+volatile unsigned int iter;
 
 void fis_start(void) {
 	// ustawienie timera 1
        	TCCR1B |= (1 << WGM12);		// tryb ctc
-	TIMSK |= (1 << OCIE1A);		// przerwanie na porownanie
-	TCCR1B |= (1 << CS10);		// bez preskalera
 	OCR1A = 422;			// dla takiej wartosci gran. jest ok. 9.7 kHz
 
 	// sekwencja startowa fis
-	DDRC |= (1 << ENA) | (1 << DATA) | (1 << CLK);
-	PORTC &= ~(1 << ENA);
-	PORTC |= (1 << DATA) | (1 << CLK);
+	DDRB |= (1 << ENA) | (1 << DATA) | (1 << CLK);
+	PORTB &= ~(1 << ENA);
+	PORTB |= (1 << DATA) | (1 << CLK);
 	_delay_ms(4.2079);
-	PORTC &= ~((1 << DATA) | (1 << CLK));
+	PORTB &= ~((1 << DATA) | (1 << CLK));
 	_delay_ms(65.3575);
-	PORTC |= (1 << DATA) | (1 << CLK);
+	PORTB |= (1 << DATA) | (1 << CLK);
 	_delay_ms(50.57);
 	return;
 }
 
 void fis_send_frame(void) {
 	iter = 0;
-	sei();
+	PORTB |= (1 << ENA);
+	_delay_ms(0.1);
+	PORTB &= ~(1 << ENA);
+	_delay_ms(0.1);
+	PORTB |= (1 << ENA);
+	TCCR1B |= (1 << CS10);		// bez preskalera
+	TIMSK |= (1 << OCIE1A);		// przerwanie na porownanie
 }
 
 int main(void)
 {
-//	DDRC=255;	
-	PORTC = 0;
-/*	
-       	TCCR1B |= (1 << WGM12);		// tryb ctc
-	TIMSK |= (1 << OCIE1A);		// przerwanie na porownanie
-	TCCR1B |= (1 << CS10);		// bez preskalera
-	OCR1A = 422;			// dla takiej wartosci gran. jest ok. 9.7 kHz
-
-	sei();				// wlacz przerwania
-*/
+	sei();
+	PORTB = 0;
 	
 	_delay_ms(1000);
 	fis_start();
@@ -63,12 +59,20 @@ int main(void)
 
 
 ISR (TIMER1_COMPA_vect) {
-	PORTC ^= (1 << CLK);
-//(dataframe[iter / 8] & (1 << iter % 8)) >> (iter % 8)
-	PORTC |= ((dataframe[iter / 8] & (1 << iter % 8)) >> (iter % 8)) << DATA; 	
-	PORTC &= ~(((dataframe[iter / 8] & (1 << iter % 8)) >> (iter % 8)) << DATA); 	
-	if(iter >= 55) {
-		cli();
+	PORTB ^= (1 << CLK);
+	if(!(iter % 2)) {
+		if ((dataframe[iter / 16] & (1 << (iter/2) % 8)))
+			PORTB &= ~(1 << DATA);
+		else
+			PORTB |= (1 << DATA);
+
+//		PORTB &= ~(((dataframe[iter / 16] & (1 << (iter/2) % 8)) >> ((iter/2) % 8)) << DATA); 	
+//		PORTB |= ((dataframe[iter / 16] & (1 << (iter/2) % 8)) >> ((iter/2) % 8)) << DATA; 	
+	}
+	iter = iter + 1;
+	if(iter >= 112) {
+		TIMSK &= ~(1 << OCIE1A);		// wyl. przerwanie na porownanie
+		PORTB &= ~(1 << ENA);
 		iter = 0;
 	}
 }
